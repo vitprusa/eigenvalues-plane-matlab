664363b703bbe89be1d7e541c186d0e5b4a20c7c
Rebecca113-ui <rebecca.tozzi@unifi.it>
2026-04-29 11:06:23 +0200

    Updated version of the code for small rectangle test
    Updated version of the code dst_laplace_grid_small_rectangle_test.m

--- dst_laplace_grid_small_rectangle_test.m
+++ dst_laplace_grid_small_rectangle_test.m
@@ -1,6 +1,6 @@
 clear all close all
 
-% L-shaped domain 
+% Small rectangle or small square
 a = 0;
 c = pi;
 d = 0;
@@ -11,24 +11,26 @@ y_range = [d, f];
 
 % Grid spacing must be the same in both directions
 % M is the number of DOF (interior grid points) in one slice (before applying the mask)
-M = 20;
+M = 23;
 h = (c-a)/(M+1);
 
-%b = 8*h;
-P = M/2;
-e = P*h;
-
+P = (M+1)/2;
+Q = (M+1)/4;
+b = P*h;
+e = Q*h;
 
 x_vec = x_range(1) : h : x_range(2);
 y_vec = y_range(1) : h : y_range(2);
 
 [X, Y] = ndgrid(x_vec, y_vec); 
 
-phi = @(x,y) indicator_rectangle(x, y, a, e, d, e);  
+% phi = @(x,y) indicator_rectangle(x, y, a, e, d, e);  % Small square
+phi = @(x,y) indicator_rectangle(x, y, a, b, d, e);  % Small rectangle
 
 % Create mask
 global_mask = phi(X, Y) > 0; 
 
+spy(global_mask)
 
 % TESTING
 %
@@ -37,10 +39,10 @@ global_mask = phi(X, Y) > 0;
 % We need a function that vanishes on boundary, and for which we can
 % calculate the laplacian
 
-mm = 5; % Must be less than or equal to P
-nn = 3; % Must be less than or equal to P
-u = @(x, y) sin(mm*x/(P*h)*pi).*sin(nn*y/(P*h)*pi);
-laplace_u = @(x, y) (-mm.^2 - nn.^2)*u(x, y)*( (pi).^2 / ((P*h).^2));
+mm = 3; % Must be less or equal to P
+nn = 2; % Must be less or equal to Q
+u = @(x, y) sin(mm*x*pi/(P*h)).*sin(nn*y*pi/(Q*h));
+laplace_u = @(x, y) (-(pi/(P*h))^2*mm.^2 - (pi/(Q*h))^2*nn.^2)*u(x, y);
 
 
 u_grid = u(X, Y);
@@ -49,8 +51,14 @@ laplace_u_grid = laplace_u(X, Y);
 u_masked_vals_grid = zeros(size(X));
 u_masked_vals_grid(global_mask) = u_grid(global_mask);  
 
+figure 
+surf(X,Y,u_masked_vals_grid)
+
 laplace_u_masked_vals_grid = zeros(size(X));
 laplace_u_masked_vals_grid(global_mask) = laplace_u_grid(global_mask); 
+
+figure 
+surf(X,Y,laplace_u_masked_vals_grid)
 
 % NUMERICAL COMPUTATION, DST
 tic
