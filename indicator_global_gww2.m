32b3442777035b19999eff535c39c9b4ec85926a
Rebecca113-ui <rebecca.tozzi@unifi.it>
2026-05-05 15:19:49 +0200

    Indicator function for GWW2 domain
    Indicator function for GWW2 domain

---
+++ indicator_global_gww2.m
@@ -0,0 +1,41 @@
+function res = indicator_global_gww2(x, y, a, b, c, d, f, g, i)
+
+    tol = 1e-15;
+
+    res1 = ind_rect(x, y); 
+    res2 = ind_iso_left_tri(x, y);
+    res3 = ind_rev_iso_left_tri(x, y);
+
+    % We have to include interior boundaries 
+    res_b = double((x > a & x < b & y == g) | (x == c & y > f & y < g));
+    
+
+    res = res1 | res2 | res3 | res_b;
+    
+    
+    function res_rect = ind_rect(x, y)
+
+        res_rect = double(x > a & x < b & y > g & y < i);
+
+    end
+
+    function res_left_tri = ind_iso_left_tri(x, y)
+
+        res_left_tri = double(x > a & x < c & y > (-x-2+tol) & y < g);
+
+    end
+
+    function res_reverse_left_tri = ind_rev_iso_left_tri(x, y)
+
+        res_reverse_left_tri = double(x > c & x < d & y > f & y < (-x+2-tol));
+
+    end
+
+end
+
+
+
+
+
+
+
