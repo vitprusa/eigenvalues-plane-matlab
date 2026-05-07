bdfb5d5595c7da474e413a127489173b977f4cf6
Rebecca113-ui <rebecca.tozzi@unifi.it>
2026-05-07 12:16:05 +0200

    Indicator function for ellipse minus a quadrant
    Indicator function for ellipse minus a quadrant

---
+++ indicator_ellipse.m
@@ -0,0 +1,16 @@
+function res = indicator_ellipse(x, y, a, b, c, e)
+
+    tol = 1e-14;
+
+
+    res1 = double(x > a & x < b & y > -(1/2)*sqrt(4-x.^2)+tol & y < e);
+
+    res2 = double(x > a & x < c & y > e & y < (1/2)*sqrt(4-x.^2)-tol);
+
+    % Adding interior boundary
+
+    resb = double(x > a & x < b & y == e);
+
+    res = res1 | res2 | resb;
+
+end
